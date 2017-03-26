d487ae6ddd7c7b67f88bc1cb04e1ed1dedbd389f
Matt Bowyer <mattbowyer@nwk-54-147.nwk.ruw.rutgers.edu>
2017-03-26 18:01:14 -0400

    Attempt

--- Optimizer.c
+++ Optimizer.c
@@ -109,8 +109,8 @@ Instruction *childI(Instruction *parent){
 			// Merge child_1 and child_2
 			parent=child;
 			parent->next = child_1;
-			for(child_1=parent;child_1->next;child_1=child_1->next);
-			child_1->next=child_2;
+			//for(child_1=parent;child_1->next;child_1=child_1->next);
+			//child_1->next=child_2;
 			return(parent);
 		}
 	}
