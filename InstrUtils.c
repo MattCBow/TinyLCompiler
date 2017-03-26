726ed2168bf77b0cf18eff78b401d7b0e7859813
Matt Bowyer <mattbowyer@Matts-Air-2.home>
2017-03-25 20:34:46 -0400

    Attempt

--- InstrUtils.c
+++ InstrUtils.c
@@ -72,7 +72,7 @@ void PrintInstructionList(FILE * outfile, Instruction * instr)
 
 	Instruction *cur;
 	for(cur=instr;cur!=NULL;cur = cur->next){
-		printf("%p --->", (void *) &parent);
+		printf("%p --->", (void *) &cur);
 		PrintInstruction(outfile, cur);
 	}
 
