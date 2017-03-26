132fa8e4f1de8afa62f4448562760e7705d75be5
Matt Bowyer <mattbowyer@nwk-54-147.nwk.ruw.rutgers.edu>
2017-03-26 18:26:21 -0400

    Attempt

--- Optimizer.c
+++ Optimizer.c
@@ -113,8 +113,8 @@ Instruction *childI(Instruction *parent){
 			//printf("%p ---> ", (void *) out);
 			parent = child;
 			child->next=child_2;
-			while(child_1->next)child_1=child_1->next;
-			while(child_2->next)child_2=child_2->next;
+			//while(child_1->next)child_1=child_1->next;
+			//while(child_2->next)child_2=child_2->next;
 			//child->next=child_2;
 			return(parent);
 		}
